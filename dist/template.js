e9c7a53fd07064b8f9433ac33a529c473077f5a4
open-fox <javayhux@gmail.com>
2026-07-04 15:54:49 +0800

    Stop tracking dist build output

--- dist/template.js
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs';
-import path from 'node:path';
-export function updatePackageName(config) {
-    const packagePath = path.join(config.targetDir, 'package.json');
-    const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
-    packageJson.name = config.projectName;
-    fs.writeFileSync(packagePath, `${JSON.stringify(packageJson, null, 2)}\n`);
-}
